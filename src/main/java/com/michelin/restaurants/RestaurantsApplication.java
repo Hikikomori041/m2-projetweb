e8af68ebc40d72edbf3b700fe7a76155f6f3a5e1
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2025-10-30 02:33:31 +0100

    ajout de 2 fichiers non essentiels

---
+++ src/main/java/com/michelin/restaurants/RestaurantsApplication.java
@@ -0,0 +1,15 @@
+package com.michelin.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsApplication.class, args);
+
+        System.out.println("===========================================================\n\n\tMichelin's new restaurants API just started !\n\n===========================================================");
+	}
+
+}
